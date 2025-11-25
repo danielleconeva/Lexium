274110f298c64d96880d5fd3e819eac62d0578fa
danielleconeva <dconewa@gmail.com>
2025-11-25 17:47:11 +0200

    expose stable callbacks from useCases hook

--- src/hooks/useCases.ts
+++ src/hooks/useCases.ts
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useCallback } from "react";
 import type { RootState, AppDispatch } from "../store/store";
 
 import {
@@ -16,23 +17,47 @@ export function useCases() {
         (state: RootState) => state.cases
     );
 
+    const loadFirmCases = useCallback(
+        (firmId: string) => {
+            return dispatch(fetchFirmCases(firmId));
+        },
+        [dispatch]
+    );
+
+    const loadPublicCases = useCallback(() => {
+        return dispatch(fetchPublicCases());
+    }, [dispatch]);
+
+    const createCaseHandler = useCallback(
+        (caseData: any) => {
+            return dispatch(createCase(caseData));
+        },
+        [dispatch]
+    );
+
+    const updateCaseHandler = useCallback(
+        (caseId: string, updatedData: any) => {
+            return dispatch(updateCase({ caseId, updatedData }));
+        },
+        [dispatch]
+    );
+
+    const deleteCaseHandler = useCallback(
+        (caseId: string) => {
+            return dispatch(deleteCase(caseId));
+        },
+        [dispatch]
+    );
+
     return {
         firmCases,
         publicCases,
         loading,
         error,
-
-        loadFirmCases: (firmId: string) =>
-            dispatch(fetchFirmCases(firmId)),
-
-        loadPublicCases: () => dispatch(fetchPublicCases()),
-
-        createCase: (caseData: any) => dispatch(createCase(caseData)),
-
-        updateCase: (caseId: string, updatedData: any) =>
-            dispatch(updateCase({ caseId, updatedData })),
-
-        deleteCase: (caseId: string) =>
-            dispatch(deleteCase(caseId)),
+        loadFirmCases,
+        loadPublicCases,
+        createCase: createCaseHandler,
+        updateCase: updateCaseHandler,
+        deleteCase: deleteCaseHandler,
     };
 }
